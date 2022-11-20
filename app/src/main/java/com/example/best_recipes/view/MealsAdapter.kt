63be2f1f9a356f982216b824d924d9f10b8c504d
azaiz haifa <haifa.azaiz@ensi-uma.tn>
2022-11-20 12:36:16 +0100

    insertion images

--- app/src/main/java/com/example/best_recipes/view/MealsAdapter.kt
+++ app/src/main/java/com/example/best_recipes/view/MealsAdapter.kt
@@ -1,15 +1,22 @@
 package com.example.best_recipes.controller
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.best_recipes.R
 import com.example.best_recipes.model.Meal
+import java.util.concurrent.Executors
 
 class MealViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var nameTextView: TextView = itemView.findViewById(R.id.textview_name)
+    var imageView : ImageView = itemView.findViewById(R.id.imageView)
 }
 
 class MealsAdapter(val meals: List<Meal>) : RecyclerView.Adapter<MealViewHolder>(){
@@ -19,6 +26,37 @@ class MealsAdapter(val meals: List<Meal>) : RecyclerView.Adapter<MealViewHolder>
     }
 
     override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
+        // Declaring executor to parse the URL
+        val executor = Executors.newSingleThreadExecutor()
+        // Once the executor parses the URL and receives the image, handler will load it in the ImageView
+        val handler = Handler(Looper.getMainLooper())
+        // Initializing the image
+        var image: Bitmap? = null
+
+        // Only for Background process (can take time depending on the Internet speed)
+        executor.execute {
+
+            // Image URL
+            val imageURL = meals.get(position).mealImage
+
+            // Tries to get the image and post it in the ImageView
+            // with the help of Handler
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+
+                // Only for making changes in UI
+                handler.post {
+                    holder.imageView.setImageBitmap(image)
+                }
+            }
+
+            // If the URL doesnot point to
+            // image or any other kind of failure
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
         holder.nameTextView.setText(meals.get(position).mealName)
     }
 
