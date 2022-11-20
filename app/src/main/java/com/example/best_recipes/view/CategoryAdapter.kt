835793706f032cd0fa81cea89aa2baac6725baa2
sana-Mas <sana.masmoudi@ensi-uma.tn>
2022-11-20 13:05:29 +0100

    image on category

--- app/src/main/java/com/example/best_recipes/view/CategoryAdapter.kt
+++ app/src/main/java/com/example/best_recipes/view/CategoryAdapter.kt
@@ -1,15 +1,22 @@
 package com.example.best_recipes.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.best_recipes.Model.Category
 import com.example.best_recipes.R
+import java.util.concurrent.Executors
 
 class CategoryViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
     var categoryTextView: TextView = itemView.findViewById(R.id.category_textview)
+    var imageView : ImageView = itemView.findViewById(R.id.imageView)
 }
 
 class CategoryAdapter(val categories: List<Category>): RecyclerView.Adapter<CategoryViewHolder>() {
@@ -20,6 +27,37 @@ class CategoryAdapter(val categories: List<Category>): RecyclerView.Adapter<Cate
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        // Declaring executor to parse the URL
+        val executor = Executors.newSingleThreadExecutor()
+        // Once the executor parses the URL and receives the image, handler will load it in the ImageView
+        val handler = Handler(Looper.getMainLooper())
+        // Initializing the image
+        var image: Bitmap? = null
+
+        // Only for Background process (can take time depending on the Internet speed)
+        executor.execute {
+
+            // Image URL
+            val imageURL = categories.get(position).urlPictureCategory
+
+            // Tries to get the image and post it in the ImageView
+            // with the help of Handler
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+
+                // Only for making changes in UI
+                handler.post {
+                    holder.imageView.setImageBitmap(image)
+                }
+            }
+
+            // If the URL doesnot point to
+            // image or any other kind of failure
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
         holder.categoryTextView.setText(categories.get(position).nameCategory)
     }
 
